f5f753702515804b9c1b8acd8242b16a52864265
Jorge <jorgegmail.com>
2025-09-07 18:39:18 +0100

    agregando cors

--- src/main/java/config/OpenApiConfig.java
+++ src/main/java/config/OpenApiConfig.java
@@ -1,8 +1,11 @@
 package config;
+
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class OpenApiConfig {
@@ -11,8 +14,21 @@ public class OpenApiConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                    .title("Mi API REST")
-                    .version("1.0")
-                    .description("Documentación de la API para mi proyecto Spring Boot"));
+                        .title("Mi API REST")
+                        .version("1.0")
+                        .description("Documentación de la API para mi proyecto Spring Boot"));
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
     }
 }
