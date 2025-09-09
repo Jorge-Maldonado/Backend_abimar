46da65860922ba93304a026dbc4646623384762b
JorgeMaldonado <m61d0n6d0@gmail.com>
2025-09-08 21:05:06 -0400

    mejora

--- src/main/java/config/SecurityConfig.java
+++ src/main/java/config/SecurityConfig.java
@@ -1,4 +1,3 @@
-
 package config;
 
 /**
@@ -7,11 +6,25 @@ package config;
  */
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable() // desactiva CSRF (para pruebas y peticiones desde Postman/Angular)
+                .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll() // permite el acceso a todos los endpoints sin login
+                )
+                .formLogin().disable() // desactiva el formulario de login por defecto
+                .httpBasic().disable(); // desactiva la autenticación básica
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
