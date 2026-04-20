dcfa6f8ca4ce840cc05714592d780495ce38f7dc
JorgeMaldonado <m61d0n6d0@gmail.com>
2026-04-20 18:40:57 -0400

    mejora

--- src/main/java/com/abimar/spring/mssql/repository/PedidoRepository.java
+++ src/main/java/com/abimar/spring/mssql/repository/PedidoRepository.java
@@ -1,7 +1,11 @@
 package com.abimar.spring.mssql.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.abimar.spring.mssql.model.Pedido;
 
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+
+    List<Pedido> findByPersonal(Long personal);
 }
